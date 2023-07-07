f637cba08a3e2da29e00489bca5931809f7eee35
Corner324 <mosenn@mail.ru>
2023-07-07 12:19:35 +0300

    Add poetry

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-token = os.getenv('token')
-chat_id_main = os.getenv('chat_id_main')
-chat_id_test = os.getenv('chat_id_test')
-target_time = os.getenv('target_time')
-url = os.getenv('url')
+token = os.environ['token']
+chat_id_main = os.environ['chat_id_main']
+chat_id_test = os.environ['chat_id_test']
+target_time = os.environ['target_time']
+url = os.environ['url']
